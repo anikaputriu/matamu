c6a3648b5b8e7e5bcceae38d3ef95a59d4c8fa7a
haffjjj <anikaputriutami@gmail.com>
2020-05-03 23:00:23 +0700

    add image slider

--- application/views/home_v.php
+++ application/views/home_v.php
@@ -63,7 +63,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <div class="banner">
                         <div class="banner_image">
                             <div class="image_bg"></div>
-                            <div class="image" style="background-image: url('<?php echo base_url() ?>assets/images/banner_image.jpg')"></div>
+                            <div class="image">
+                                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img class="d-block w-100" src="<?php echo base_url() ?>assets/images/banner_image_1.jpg" alt="Second slide">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img class="d-block w-100" src="<?php echo base_url() ?>assets/images/banner_image_2.jpeg" alt="Second slide">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img class="d-block w-100" src="<?php echo base_url() ?>assets/images/banner_image_3.jpeg" alt="Second slide">
+                                        </div>
+                                    </div>
+                                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -163,6 +185,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <p>Copyright 2020 Mata.mu . All Rights Reserved</p>
         </div>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script>
             const BASE_URL = "<?php echo base_url() ?>"
         </script>
